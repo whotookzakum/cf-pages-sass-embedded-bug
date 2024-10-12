1869fec02d5805c9e73ba32135181f3c0b78d0fe
Zakum <mirveo@gmail.com>
2024-10-12 09:57:17 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "./styles.scss"
+</script>
+
+<h1>CF pages bug</h1>
+<p>This page will build with SvelteKit, however it stalls out just before deploying when using CF pages.</p>
